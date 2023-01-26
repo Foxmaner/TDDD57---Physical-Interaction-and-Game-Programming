840a0bd62b7243396af667bd8757edd243945212
Eskil Brännerud <eskbr129@student.liu.se>
2023-01-26 14:18:16 +0100

    fooling around

--- Control.gd
+++ Control.gd
@@ -2,6 +2,7 @@ extends Control
 
 var client = WebSocketClient.new()
 var url = "ws://localhost:5000"
+onready var handIcon = $Sprite
 
 func _ready():
 	client.connect("data_received", self, "_on_data_recieved")
@@ -15,12 +16,38 @@ func _process(_delta):
 	client.poll()
 	
 var dict = null
+
+func isHandClosed(handData):
+	var middleFingerTop = handData[12]
+	var middleFingerRoot = handData[9]
+	
+	if(middleFingerTop["y"]>middleFingerRoot["y"]):
+		return true
+	else:
+		return false
+
+func getHandPositionRelative(x,y):
+	return Vector2(x*get_viewport().size.x,y*get_viewport().size.y)
+
+
 func _on_data_recieved(): 
 	var payload = client.get_peer(1).get_packet().get_string_from_utf8()
 	var dict = JSON.parse(payload)
 	#Depending on the dict, the TYPE: varable will be FACE_DETECT, FACE_TRACK, POSE, HANDS and then the data will be in "DATA" key
 	#e.g. dict.DATA == "FACE_TRACK"
-	$TextEdit2.text = str(dict.result)
+	$TextEdit2.text = str(dict.result["DATA"]["landmark"][1])
+	
+	var handposition = getHandPositionRelative(dict.result["DATA"]["landmark"][1]["x"],dict.result["DATA"]["landmark"][1]["y"])
+	
+	
+	handIcon.position = handposition
+	
+	if(isHandClosed(dict.result["DATA"]["landmark"])):
+		handIcon.modulate = Color(0,0,1)
+	else:
+		handIcon.modulate = Color(1,1,1)
+	
+	
 	
 	
 func send(data): 
