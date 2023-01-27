e4e54e493cbae0d32ffff2339b1e6b01d88d8772
Eskil Brännerud <eskbr129@student.liu.se>
2023-01-27 16:35:15 +0100

    done with lab 1?

--- Control.gd
+++ Control.gd
@@ -2,7 +2,14 @@ extends Control
 
 var client = WebSocketClient.new()
 var url = "ws://localhost:5000"
-onready var handIcon = $Sprite
+onready var handIcon = $HandArea/Sprite
+onready var handArea = $HandArea
+onready var boxArea = $BoxArea
+var openHand = preload("res://pics/open_hand.png")
+var closedHand = preload("res://pics/closed_hand.png")
+
+var isHandOnBox = false
+var handClosed = false
 
 func _ready():
 	client.connect("data_received", self, "_on_data_recieved")
@@ -15,6 +22,12 @@ func _ready():
 func _process(_delta):
 	client.poll()
 	
+func _physics_process(delta):
+	if(handClosed && isHandOnBox):
+		print("TRUUUE!")
+		boxArea.position = handArea.position
+	pass
+	
 var dict = null
 
 
@@ -26,9 +39,9 @@ func isHandClosed(handData):
 	
 	var handToFingerTop = Vector2(handRoot["x"]-middleFingerTop["x"],handRoot["y"]-middleFingerTop["y"])
 	var handToFingerRoot = Vector2(handRoot["x"]-middleFingerRoot["x"],handRoot["y"]-middleFingerRoot["y"])
-	print(str(handToFingerRoot.length())  + ">" + str(handToFingerTop.length()))
+	#print(str(handToFingerRoot.length())  + ">" + str(handToFingerTop.length()))
 	
-	if(handToFingerRoot.length() > handToFingerTop.length()*1.3):
+	if(handToFingerRoot.length() > handToFingerTop.length()*1.1):
 		return true
 	else:
 		return false
@@ -37,7 +50,7 @@ func isHandClosed(handData):
 func getHandPositionRelative(x,y):
 	return Vector2(x*get_viewport().size.x,y*get_viewport().size.y)
 
-
+	
 func _on_data_recieved(): 
 	var payload = client.get_peer(1).get_packet().get_string_from_utf8()
 	var dict = JSON.parse(payload)
@@ -47,14 +60,16 @@ func _on_data_recieved():
 	
 	#Change position of sprite icon
 	var handposition = getHandPositionRelative(dict.result["DATA"]["landmark"][9]["x"],dict.result["DATA"]["landmark"][9]["y"])
-	handIcon.position = handposition
+	handArea.position = handposition
 	
 	#Change color of hand if its closed
 	if(isHandClosed(dict.result["DATA"]["landmark"])):
-		handIcon.modulate = Color(0,0,1)
+		handIcon.texture = closedHand;
+		handClosed = true;
+		
 	else:
-		handIcon.modulate = Color(1,1,1)
-	
+		handIcon.texture = openHand;
+		handClosed = false;
 	
 	
 	
@@ -64,4 +79,16 @@ func send(data):
 
 func _on_Button_pressed():
 	send($TextEdit.text)
+	
+
+
+
+
+func _on_HandArea_area_entered(area):
+	isHandOnBox=true;
+	
+
+
+func _on_HandArea_area_exited(area):
+	isHandOnBox=false;
 	
