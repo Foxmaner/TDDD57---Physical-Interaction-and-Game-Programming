9ba9a598d8f475c044b17d739c9cddcde8e7a20b
Lucas Granberg <lucgr483@student.liu.se>
2023-02-15 14:58:47 +0100

    Created Node for BodyInterpreter

---
+++ BodyInterpreter.gd
@@ -0,0 +1,110 @@
+extends Node
+
+var tiltHead = "Neutral"
+var tiltPositions = ["Right", "Neutral", "Left"]
+var tiltHeadRaw = 0
+
+var pitchHead = "Neutral"
+var pitchPositions = ["Up", "Neutral", "Down"]
+var pitchHeadLeftEar = 0
+var pitchHeadRightEar = 0
+
+var mouthOpen = "Closed"
+var mouthPositions = ["Closed", "Open"]
+var mouthOpenRaw = 0
+
+var faceLandMarks = {}
+
+# -1 = right, 0 = neutral, 1 left
+func discreetTilt(rawTilt):
+	if (rawTilt < -10):
+		return -1
+	elif (rawTilt > 10):
+		return 1
+	else:
+		return 0
+		
+# -1 = up, 0 = neutral, 1 down
+func discreetPitch(rawPitch):
+	
+	if (rawPitch > -10):
+		return -1
+	elif (rawPitch < -40):
+		return 1
+	else:
+		return 0
+	"""
+	if (rawPitch > 0):
+		return -1
+	elif (rawPitch > -70):
+		return 1
+	else:
+		return 0
+	"""
+		
+# 0 = closed, 1 = open
+func discreetMouth(rawMouth):
+	if (rawMouth < 40):
+		return 0
+	else:
+		return 1
+
+func createLandmarks(faceData):
+	var allLandmarks = faceData.result["DATA"]["landmark"]
+	faceLandMarks["topHead"] = extractVector2(allLandmarks[10])
+	faceLandMarks["bottomHead"] = extractVector2(allLandmarks[152])
+	faceLandMarks["topLip"] = extractVector2(allLandmarks[13])
+	faceLandMarks["bottumLip"] = extractVector2(allLandmarks[14])
+	faceLandMarks["leftMouth"] = extractVector2(allLandmarks[78])
+	faceLandMarks["rightMouth"] = extractVector2(allLandmarks[308])
+	faceLandMarks["rightEar"] = extractVector2(allLandmarks[454])
+	faceLandMarks["leftEar"] = extractVector2(allLandmarks[234])
+	faceLandMarks["noseTip"] = extractVector2(allLandmarks[94])
+	
+func extractVector2(landmark):
+	return Vector2(landmark["x"], landmark["y"])
+
+func isMouthOpen(faceLandMarks):
+	var vectorLeftMothToLip = faceLandMarks["bottumLip"]-faceLandMarks["topLip"]
+	#print(str(vectorLeftMothToLip.length()))
+	var topLip = faceLandMarks["topLip"]-faceLandMarks["leftMouth"]
+	var buttomLip = faceLandMarks["bottumLip"]-faceLandMarks["leftMouth"]
+	mouthOpenRaw = rad2deg(topLip.angle_to(buttomLip))
+	
+func headPitch(faceLandMarks):
+	"""
+	var vectorEarToEar = faceLandMarks["leftEar"]-faceLandMarks["rightEar"]
+	var vectorEarToNose = faceLandMarks["leftEar"]-faceLandMarks["noseTip"]
+	pitchHeadRaw = rad2deg(vectorEarToEar.angle_to(vectorEarToNose))
+	"""
+	var vectorNoseLeftEar = faceLandMarks["leftEar"] - faceLandMarks["noseTip"]
+	var vectorNoseRightEar = faceLandMarks["rightEar"] - faceLandMarks["noseTip"]
+	var vectorBetweenEars = faceLandMarks["leftEar"] - faceLandMarks["rightEar"]
+	
+	pitchHeadLeftEar = rad2deg(vectorNoseLeftEar.angle_to(vectorBetweenEars))
+	pitchHeadRightEar = -rad2deg(vectorNoseRightEar.angle_to(-vectorBetweenEars))
+	
+func headTilt(faceLandMarks):
+	var vectorTopToButtomHead = faceLandMarks["topHead"]-faceLandMarks["bottomHead"]
+	tiltHeadRaw = rad2deg(vectorTopToButtomHead.angle_to(Vector2.UP))
+
+func toString():
+	return "Tilt: %s (%s) \nPitch: %s (%s, %s) \nMouth: %s (%s) \n" % \
+	 	[tiltHead, tiltHeadRaw, pitchHead, pitchHeadLeftEar, 
+		 pitchHeadRightEar, mouthOpen, mouthOpenRaw]
+
+func interpretData(dict):
+	createLandmarks(dict)
+
+	isMouthOpen(faceLandMarks)
+	headPitch(faceLandMarks)
+	headTilt(faceLandMarks)
+	
+	tiltHead = tiltPositions[discreetTilt(tiltHeadRaw) + 1]
+	mouthOpen = mouthPositions[discreetMouth(mouthOpenRaw)]
+	
+	var pitch = discreetPitch(pitchHeadLeftEar)
+	if (pitch == discreetPitch(pitchHeadRightEar)):
+		pitchHead = pitchPositions[pitch + 1]
+	else:
+		pitchHead = "CONFLICT"
